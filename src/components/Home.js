79aa9f7004f3cd29d7b5fcbf9d1b0e521e177f15
RadhikaManjunath <radhikamanjunath34@gmail.com>
2023-05-18 12:30:38 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home()
 {
     return(
     <div class="bg-dark">
-        <Navbar style={{height:"600px",width:"600px"}}/>
-        <Color style={{height:"600px",width:"600px"}}/>
+        <Navbar />
+        <Color />
     </div>
     )
 }
